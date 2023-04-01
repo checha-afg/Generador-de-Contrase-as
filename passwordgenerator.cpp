84619b848efb06da69fef5cb183535719af05a78
pscherios <99372151+PsChecha@users.noreply.github.com>
2023-04-01 12:35:27 -0600

    Initial commit

---
+++ passwordgenerator.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+const char alphanum[] = "0123456789!@#$%^*_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int string_length = sizeof(alphanum);
+
+int main(){
+	int length = 0;
+	cout << "Ingrese la cantidad de caracteres de la contrasena: ";
+	cin >> length;
+	srand(time(0));
+	cout << "Contrasena generada: ";
+	for (int i = 0;i < length;i++){
+		cout << alphanum[rand() % string_length];
+	}
+	return 0;
+}
