70375fc2b73b51c99e444acd9b8e77b7de0417a8
pscherios <99372151+PsChecha@users.noreply.github.com>
2023-06-02 08:59:47 -0600

    v1.3
    Pequeñas mejoras

--- passwordgenerator.cpp
+++ passwordgenerator.cpp
@@ -7,15 +7,32 @@ using namespace std;
 const char alphanum[] = "0123456789!@#$%^*_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 int string_length = sizeof(alphanum);
 
-int main(){
-	int length = 0;
-	cout << "Ingrese la cantidad de caracteres de la contrasena: ";
-	cin >> length;
-	srand(time(0));
-	cout << "Contrasena generada: ";
-	for (int i = 0;i < length;i++){
-		cout << alphanum[rand() % string_length];
-	}
-	return 0;
-	system("pause");
+void generarContrasenas(int cantidad, int longitud) {
+    srand(time(0));
+    for (int j = 0; j < cantidad; j++) {
+        cout << "Contraseña generada " << j + 1 << ": ";
+        for (int i = 0; i < longitud; i++) {
+            cout << alphanum[rand() % string_length];
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int cantidad = 0;
+    int longitud = 0;
+    char opcion;
+
+    do {
+        cout << "Ingrese la cantidad de contraseñas a generar: ";
+        cin >> cantidad;
+        cout << "Ingrese la longitud de cada contraseña: ";
+        cin >> longitud;
+        generarContrasenas(cantidad, longitud);
+
+        cout << "¿Desea generar más contraseñas? (S/N): ";
+        cin >> opcion;
+    } while (opcion == 'S' || opcion == 's');
+
+    return 0;
 }
